4de8496e9f74f04753ad701831a8e2e151fccb9f
Adam Stępień <vesemir2k@gmail.com>
2024-07-26 22:45:44 +0200

    📝 (calculatorAgeForm.scss): Update styles for the age calculator form to improve layout and design 📝 (CalculatorAgeForm.vue): Add submit event listener to the form and a button with an arrow icon for submission functionality

--- src/components/CalculatorAgeForm.vue
+++ src/components/CalculatorAgeForm.vue
@@ -1,8 +1,11 @@
 <template>
-  <form class="form">
+  <form class="form" @submit.prevent="console.log('click')">
     <CalculatorAgeFormInput name="day" labelText="DAY" placeholder="DD" />
     <CalculatorAgeFormInput name="month" labelText="MONTH" placeholder="MM" />
     <CalculatorAgeFormInput name="year" labelText="YEAR" placeholder="YYYY" />
+    <button class="form__button button">
+      <img class="form__button__icon" src="../assets/images/icon-arrow.svg" alt="" />
+    </button>
   </form>
 </template>
 
