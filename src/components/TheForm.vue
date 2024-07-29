be138d9f14cc42387455708853be95fd16426513
Adam Stępień <vesemir2k@gmail.com>
2024-07-29 23:58:54 +0200

    ✨ (TheForm.vue): refactor TheForm component to use Vue 3 Composition API with reactive variables and dataShow method for form submission functionality

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,20 +1,32 @@
 <template>
-  <form class="form" @submit.prevent="console.log('click')">
-    <FormGroup name="day" labelText="DAY" placeholder="DD" />
-    <FormGroup name="month" labelText="MONTH" placeholder="MM" />
-    <FormGroup name="year" labelText="YEAR" placeholder="YYYY" />
+  <form class="form" @submit.prevent="dataShow">
+    <FormGroup v-model="day" name="day" labelText="DAY" placeholder="DD" />
+    <FormGroup v-model="month" name="month" labelText="MONTH" placeholder="MM" />
+    <FormGroup v-model="year" name="year" labelText="YEAR" placeholder="YYYY" />
     <FormLine />
     <FormButton />
   </form>
 </template>
 
 <script>
+import { ref } from 'vue'
 import FormGroup from './FormGroup.vue'
 import FormLine from './FormLine.vue'
 import FormButton from './FormButton.vue'
 
 export default {
   name: 'TheForm',
-  components: { FormGroup, FormLine, FormButton }
+  components: { FormGroup, FormLine, FormButton },
+  setup() {
+    const day = ref('')
+    const month = ref('')
+    const year = ref('')
+
+    const dataShow = () => {
+      console.log(day.value, month.value, year.value)
+    }
+
+    return { day, month, year, dataShow }
+  }
 }
 </script>
